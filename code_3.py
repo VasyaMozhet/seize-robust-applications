324bfab624b873de130d96f39ba926d524d820c3
VasyaMozhet <158567931+VasyaMozhet@users.noreply.github.com>
2024-03-29 07:44:44 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,40 @@
+    data = generate_random_data()
+
+    for item in data:
+def main():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+
+
+    main()
+        print(f"Random Number: {item}")
+def generate_random_data():
+    return data    data = generate_random_data()
+
+def main():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+import random
+def generate_random_data():
+    for item in data:
+    main()    data = generate_random_data()
+    main()
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+def main():
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+if __name__ == "__main__":
+import random
